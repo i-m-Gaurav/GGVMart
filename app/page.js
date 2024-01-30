9eaff13507ae6f6417cbfadeb571968a96f8638c
Yashraj Yadav <yyashraj05@gmail.com>
2024-01-30 20:30:22 +0000

    Implement Firebase Auth

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
+'use client'
 import Nav from "@/components/Nav";
 import CardContainer from "@/components/CardContainer";
+import { AuthContextProvider } from "./context/AuthContext";
 
 export default function Home() {
  return(
   <>
+  <AuthContextProvider>
     <Nav />
-    <CardContainer  />  
+    <CardContainer  /> 
+    </AuthContextProvider>
   </>
  )
 }
